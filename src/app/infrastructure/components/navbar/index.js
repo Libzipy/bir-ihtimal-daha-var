3da4f00487c1462eaf3ec00d409ddb718dd3b5ee
Furkan Portakal <furkanportakalx@gmail.com>
2021-05-12 15:40:46 +0300

    :fire: navbar added && styled

--- src/app/infrastructure/components/navbar/index.js
+++ src/app/infrastructure/components/navbar/index.js
@@ -3,8 +3,15 @@ import { Logo } from '../../../assets'
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={Logo} />
+    <div className="navbar-container">
+      <div className="navbar-content">
+        <div className="navbar-content-left">
+          <img className="logo" src={Logo} />
+        </div>
+        <div className="navbar-content-right">
+          <button className="btn-lg navbar-button">Hadi Başlayalım</button>
+        </div>
+      </div>
     </div>
   )
 }
