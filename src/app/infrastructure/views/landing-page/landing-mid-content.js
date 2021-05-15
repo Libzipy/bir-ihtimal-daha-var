6dfee07ee997523ab472dd1848c7541f5d905de7
Furkan Portakal <furkanportakalx@gmail.com>
2021-05-15 13:46:36 +0300

    landing page process

--- src/app/infrastructure/views/landing-page/landing-mid-content.js
+++ src/app/infrastructure/views/landing-page/landing-mid-content.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { LandingOne, LandingTwo } from '../../../assets'
 
 const LandingMidContent = () => {
-  return <div className="landing-container-mid">landing-container-mid</div>
+  return (
+    <div className="landing-container-mid">
+      <div className="landing-wrapper">
+        <div className="landing-content-one">
+          <img className="landing-content-one-img" src={LandingOne} />
+          <div className="landing-content-one-right">
+            <div className="landing-content-one-right-empty"></div>
+            <div className="landing-content-one-right-content">
+              Butonuna tiklayarak rastgele veri seti olusturabilirsiniz.
+            </div>
+          </div>
+        </div>
+        <div className="landing-content-two">
+          <div className="landing-content-two-left">
+            <div className="landing-content-two-left-content">
+              Butonuna tiklayarak rastgele veri seti olusturabilirsiniz.
+            </div>
+            <div className="landing-content-two-left-empty"></div>
+          </div>
+          <img className="landing-content-two-img" src={LandingTwo} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default LandingMidContent
