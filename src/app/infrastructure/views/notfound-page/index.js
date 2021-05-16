993912210534db8a8a708ad419e60e17d100d059
Furkan Portakal <furkanportakalx@gmail.com>
2021-05-16 18:52:21 +0300

    404 page added

--- src/app/infrastructure/views/notfound-page/index.js
+++ src/app/infrastructure/views/notfound-page/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Navbar, Footer } from '../../components'
 
 const NotFoundPage = () => {
   return (
     <>
       <Navbar />
-      <div className="center notfound-container">opps</div>
+      <div className="center notfound-container">
+        <div className="notfound-wrapper center">
+          <i className="far fa-compass"></i>
+          <h1>Hey Hey Hey!</h1>
+          <p>Bu sayfa bulunamıyor.</p>
+          <button className="btn-lg submit-option">
+            <Link to="/">☞ Anasayfaya dön</Link>
+          </button>
+        </div>
+      </div>
       <Footer />
     </>
   )
