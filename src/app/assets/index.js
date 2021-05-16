75683b3684e28fcbc1656f8eb754a56374957f4a
Furkan Portakal <furkanportakalx@gmail.com>
2021-05-16 18:41:20 +0300

    contact page added

--- src/app/assets/index.js
+++ src/app/assets/index.js
@@ -3,5 +3,8 @@ import Mockup from './mockup.png'
 import LandingOne from './landing-one.svg'
 import LandingTwo from './landing-two.svg'
 import TimelineIcon from './icons/timeline.svg'
+import Porti from './port.svg'
+import Bijon from './bijon.svg'
+import Emir from './emir.svg'
 
-export { Logo, Mockup, TimelineIcon, LandingOne, LandingTwo }
+export { Logo, Mockup, TimelineIcon, LandingOne, LandingTwo, Porti, Bijon, Emir }
