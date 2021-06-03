7cdb9c2549787530733c1313016e9030c2d993f2
Furkan Portakal <furkanportakalx@gmail.com>
2021-06-04 01:39:11 +0300

    historgram finished

--- src/app/infrastructure/views/result-page/index.js
+++ src/app/infrastructure/views/result-page/index.js
@@ -30,8 +30,7 @@ const ResultPage = () => {
   const selectedData = data.data.data.split(',').map(Number)
   const converN = parseInt(n)
   const converR = parseInt(r)
-
-  const grup = 3
+  const grup = localStorage.getItem('harmonik')
 
   const result =
     selected == 14
@@ -63,60 +62,69 @@ const ResultPage = () => {
       : selected == 1
       ? modeCount(selectedData)
       : 0
-  
-      const veri = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: 'Frekans',
-            data: result, // Sonuç Buraya 
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      };
-      
-      const options = {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
+  let veri, options
+  if (selected == 14) {
+    veri = {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: 'Frekans',
+          data: result, // Sonuç Buraya
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
           ],
-        },
-      };
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }
+      ]
+    }
+
+    options = {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  }
+
   return (
     <>
       <Navbar />
       <div className="center">
         <CheckoutSteps step1 step2 step3 step4 />
       </div>
-      <div className="result-container">
-        {
+
+      {selected == 14 ? (
+        <div className="center">
+          <div style={{ width: '800px', height: 'auto', marginTop: '1rem' }}>
+            <Bar data={veri} options={options} />
+          </div>
+        </div>
+      ) : (
+        <div className="result-container">
           <div className="result-text">
             <div className="result-title">Sonuç</div>
             {result}
           </div>
-        }
-      </div>
-      <Bar data={veri} options={options} />
+        </div>
+      )}
       <Footer />
     </>
   )
