c624a333459c8009fe550274b35312db8cb1bf03
BijanE <bijanetessam@yahoo.com>
2021-06-03 23:48:23 +0300

    updated Graphs

--- src/app/infrastructure/views/result-page/index.js
+++ src/app/infrastructure/views/result-page/index.js
@@ -30,9 +30,8 @@ const ResultPage = () => {
   const selectedData = data.data.data.split(',').map(Number)
   const converN = parseInt(n)
   const converR = parseInt(r)
-  console.log('selectedData', selectedData)
 
-  const grup = 2
+  const grup = 3
 
   const result =
     selected == 14
@@ -64,16 +63,45 @@ const ResultPage = () => {
       : selected == 1
       ? modeCount(selectedData)
       : 0
-
-  const getRandomColor = () => {
-    let letters = '0123456789ABCDEF'
-    let color = '#'
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)]
-    }
-    return color
-  }
-
+  
+      const veri = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: 'Frekans',
+            data: result, // Sonuç Buraya 
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+      
+      const options = {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      };
   return (
     <>
       <Navbar />
@@ -88,6 +116,7 @@ const ResultPage = () => {
           </div>
         }
       </div>
+      <Bar data={veri} options={options} />
       <Footer />
     </>
   )
